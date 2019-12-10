192eca503a43c561e672d2fd338d592d8915a8c3
Gov Suresh <govind.suresh1@gmail.com>
2019-12-09 22:37:59 -0500

    updated NN_model and ML model ipynbs. Final flask app python script to run webapp. Updated html for front end website. Added various other resources for the website

--- flask_app/model_app.py
+++ flask_app/model_app.py
@@ -1,4 +1,5 @@
 import base64
+from flask_cors import CORS, cross_origin
 import numpy as np
 import pandas as pd 
 from ast import literal_eval
@@ -16,14 +17,18 @@ from flask import jsonify
 from flask import Flask
 
 app = Flask(__name__)
+cors = CORS(app)
 
 def get_model():
     global model
-    model = load_model('INPUT MODEL FILENAME')
+    model = load_model('../NN_model/baseline-LSTM.h5')
     print(" MODEL LOADED")
 
 def text_padder(text, tokenizer):
-    return sequence.pad_sequences(tokenizer.texts_to_sequences(text), maxlen=250)
+    # The model needs to take in strings which have been tokenized and put in a nested list
+    text = [text]
+     # uses keras tokenizer to convert words into word indexes and pad sequences to 300
+    return sequence.pad_sequences(tokenizer.texts_to_sequences(text), maxlen=300)
 
 def train_tokenizer(train_data, vocab_size):
     # Use Keras tokenizer to create vocabulary dictionary 
@@ -35,7 +40,7 @@ def train_tokenizer(train_data, vocab_size):
 
 def load_training_data():
     global training_data
-    training_data = pd.read_csv('../data/train_for_nn.csv',converters={"comment_text_clean2": literal_eval)
+    training_data = pd.read_csv('../data/nn_tokenizer_fit_data.csv',converters={"comment_text_clean2": literal_eval})
     training_data = training_data['comment_text_clean2']
     return training_data
 
@@ -50,14 +55,15 @@ tokenizer = train_tokenizer(train_data, 200000)
 def predict():
     message = request.get_json(force=True)
     comment = message['comment']
+    
     processed_comment = text_padder(comment, tokenizer)
 
     prediction = model.predict(processed_comment).tolist()
 
     response = {
         'prediction' : {
-            non-toxic: prediction[0][0]
-            toxic: prediction[0][1]
+            'non_toxic': prediction[0][0],
+            'toxic': prediction[0][1]
 
         }
     }
